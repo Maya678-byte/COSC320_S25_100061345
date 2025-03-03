6fcea57b68a235191fd203b3923cce12306dced5
Maya678-byte <elfoulymaya182@gmail.com>
2025-03-03 09:49:30 +0400

    Add files via upload

---
+++ Week8/17_tests/tes1.rs
@@ -0,0 +1,26 @@
+// Tests are important to ensure that your code does what you think it should
+// do.
+
+fn is_even(n: i64) -> bool {
+    n % 2 == 0
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    // TODO: Import `is_even`. You can use a wildcard to import everything in
+    // the outer module.
+
+// Import everything from the outer module (`super`) using a wildcard.
+    use super::*;
+
+    #[test]
+    fn you_can_assert() {
+        assert!(is_even(0));
+        assert!(!is_even(-1));
+        //Assert `false` using the negation operator `!`.
+    }
+}

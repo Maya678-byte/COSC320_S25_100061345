bfb0b6eda5c3f78041dd2e8705b90b40965ac34a
Maya678-byte <elfoulymaya182@gmail.com>
2025-03-17 10:02:26 +0400

    Update mod1.rs

--- Week10/10_modules/mod1.rs
+++ Week10/10_modules/mod1.rs
@@ -4,7 +4,7 @@ mod sausage_factory {
     fn get_secret_recipe() -> String {
         String::from("Ginger")
     }
-// Added `pub` before `fn` to make the function accessible outside the module.
+
     pub fn make_sausage() {
         get_secret_recipe();
         println!("sausage!");
